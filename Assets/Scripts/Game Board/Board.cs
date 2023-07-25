cd0990f7758fa1f19342ebce5990f1efae00f54b
CollinTift <collin.tift@gmail.com>
2023-07-25 17:32:44 -0400

    3D project, removed old 2D

---
+++ Assets/Scripts/Game Board/Board.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+  [Header("Board Size")]
+  public int boardSizeX = 10;
+  public int boardSizeY = 10;
+
+  //2D array of Terrains set to size of boardSize
+
+  private void Start() {
+    //initialize terrain array
+  }
+}
